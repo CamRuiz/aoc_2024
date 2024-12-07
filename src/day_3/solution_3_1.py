49e7ec9c93f3dbaad499476f6c8d524ff9f56b28
CamRuiz <cameron_ruiz@hotmail.co.uk>
2024-12-07 21:11:55 +0000

    Add solution for problem 3.1

---
+++ src/day_3/solution_3_1.py
@@ -0,0 +1,23 @@
+import re
+
+from src.utilities.timer import run_timed
+
+def solution() -> None:
+    with open('src/day_3/input_3_1.txt', 'r') as file:
+        input_text = file.read()
+
+    result = 0
+    mults_to_perform = (re.findall(r'mul\(\d+,\d+\)', input_text))
+
+    for mult in mults_to_perform:
+        x, y = map(int, re.findall(r'\d+', mult))
+        result += x * y
+    print(result)
+
+
+def main() -> None:
+    run_timed(solution)
+
+
+if __name__ == "__main__":
+    main()
